adfeb0683f6d7f02c70ffeca2ab2bbbb2c46c642
trihuynh <77220677+trihuynh1811@users.noreply.github.com>
2024-02-28 08:53:05 +0700

    add repos and services

---
+++ Repository/BaseRepository/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.BaseRepository
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly ASPContext _context;
+        private DbSet<T> _dbSet;
+
+        public BaseRepository()
+        {
+            _context = new ASPContext();
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public virtual List<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Repository/Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
+using Repository.BaseRepository;
+
+namespace Repository.Interface
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+    }
+}
