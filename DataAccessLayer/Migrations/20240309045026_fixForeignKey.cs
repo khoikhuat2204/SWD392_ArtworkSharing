ccedb76de6fa9117352fa7ad33977901045a16ae
Bui Hoang Hai <buihoanghai2405@gmail.com>
2024-03-09 11:51:47 +0700

    foreign key migrations

---
+++ DataAccessLayer/Migrations/20240309045026_fixForeignKey.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class fixForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Artworks_ArtworkTypes_ArtworkTypeId",
+                table: "Artworks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Artworks_ArtworkTypeId",
+                table: "Artworks");
+
+            migrationBuilder.DropColumn(
+                name: "ArtworkTypeId",
+                table: "Artworks");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artworks_TypeId",
+                table: "Artworks",
+                column: "TypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Artworks_ArtworkTypes_TypeId",
+                table: "Artworks",
+                column: "TypeId",
+                principalTable: "ArtworkTypes",
+                principalColumn: "artworkType_Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Artworks_ArtworkTypes_TypeId",
+                table: "Artworks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Artworks_TypeId",
+                table: "Artworks");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ArtworkTypeId",
+                table: "Artworks",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artworks_ArtworkTypeId",
+                table: "Artworks",
+                column: "ArtworkTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Artworks_ArtworkTypes_ArtworkTypeId",
+                table: "Artworks",
+                column: "ArtworkTypeId",
+                principalTable: "ArtworkTypes",
+                principalColumn: "artworkType_Id");
+        }
+    }
+}
