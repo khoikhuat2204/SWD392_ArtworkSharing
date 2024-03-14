75c21dc387f004200c8477b86c4cdaa3fd8460db
walle <buihoanghai2405@gmail.com>
2024-03-14 15:56:24 +0700

    add price into artwork

--- DataAccessLayer/DTOs/RequestDTO/UpdateArtworkDTO.cs
+++ DataAccessLayer/DTOs/RequestDTO/UpdateArtworkDTO.cs
@@ -4,6 +4,7 @@ public class UpdateArtworkDTO
 {
     public string Name { get; set; }
     public string? Description { get; set; }
+    public decimal? Price { get; set; }
     public int TypeId { get; set; }
     public bool IsDeleted { get; set; }
 }
--- DataAccessLayer/DTOs/RequestDTO/UploadArtworkDTO.cs
+++ DataAccessLayer/DTOs/RequestDTO/UploadArtworkDTO.cs
@@ -8,6 +8,7 @@ public class UploadArtworkDTO
 {
     public string Name { get; set; }
     public string Description { get; set; }
+    public decimal? Price { get; set; }
    
     public int TypeId { get; set; }
     public ArtworkStatus ArtworkStatus = ArtworkStatus.Available;
