51ad0123d350ee40127f5fbf8fd4829bedf3a80d
luosrevlis <tuanminhphamtdn@gmail.com>
2024-03-19 22:21:09 +0700

    delete unnecessary apis, add get package by id

--- Services/Interface/IPackageService.cs
+++ Services/Interface/IPackageService.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DataAccessLayer.Models;
+﻿using DataAccessLayer.Models;
 
 namespace Services.Interface
 {
     public interface IPackageService
     {
         public List<Package> GetAll();
+
+        public Package? GetById(int id);
 
         public void Add(Package package);
 
